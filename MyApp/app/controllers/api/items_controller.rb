64004394d2bb5990aedce303bb8b7299e7bef8c5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-09 23:09:35 -0500

    Update items_controller.rb

--- MyApp/app/controllers/api/items_controller.rb
+++ MyApp/app/controllers/api/items_controller.rb
@@ -1,6 +1,7 @@
 # app/controllers/api/items_controller.rb
 class Api::ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   end
   
