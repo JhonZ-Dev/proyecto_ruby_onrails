4af9a7a0897a55b136530dc11827755a12c05c1f
Jhon Zambrano <79710173+JhonZ-Dev@users.noreply.github.com>
2023-12-14 23:41:15 -0500

    Update items_controller.rb

--- MyApp/app/controllers/api/items_controller.rb
+++ MyApp/app/controllers/api/items_controller.rb
@@ -3,14 +3,6 @@ class Api::ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
 
-  def index
-    @items = Item.all
-    render json: @items
-  end
-
-  def show
-    render json: @item
-  end
 
   def new
     @item = Item.new
